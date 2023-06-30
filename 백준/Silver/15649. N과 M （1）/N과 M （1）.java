936908571c9da2a8f3f5dc29fb5edb052578094e
qgw13467 <51719533+qgw13467@users.noreply.github.com>
2023-06-30 13:40:14 +0900

    [Silver III] Title: N과 M (1), Time: 2112 ms, Memory: 61636 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+
+	static int[] arr;
+	static int[] v;
+	static int n;
+	static int c;
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		n = scanner.nextInt();
+		c = scanner.nextInt();
+		arr = new int[c];
+		v = new int[n + 1];
+
+		fuc(0);
+
+	}
+
+	static void fuc(int cnt) {
+
+		if (cnt == c) {
+
+			for (int i = 0; i < c; i++) {
+				System.out.print(arr[i] + " ");
+			}
+			System.out.println();
+			return;
+		}
+
+		for (int i = 1; i <= n; i++) {
+			if (v[i] == 1) {
+				continue;
+			}
+			arr[cnt] = i;
+			v[i] = 1;
+
+			fuc(cnt + 1);
+
+			v[i] = 0;
+		}
+
+	}
+}
